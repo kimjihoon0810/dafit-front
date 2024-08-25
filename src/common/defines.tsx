c8aff5e43010773eeb6235e18fc8f02dc52150ae
kimjihoon0810 <jksc32555@gmail.com>
2024-08-25 20:30:54 +0900

    헤더 초안 작성

---
+++ src/common/defines.tsx
@@ -0,0 +1,14 @@
+export const GNB = [
+  {
+    title: "시설 찾기",
+    path: "/gyms",
+  },
+  {
+    title: "트레이너",
+    path: "/trainers",
+  },
+  {
+    title: "지도 검색",
+    path: "/map",
+  },
+];
